27c2d143789dbe0e322b8c0a26709da221f08fdb
Jes015 <jes.18.sin.15@gmail.com>
2024-03-02 08:46:49 -0500

    feat: add Header component (#3)
    resolves #3

---
+++ src/config/app.config.ts
@@ -0,0 +1,9 @@
+export const appConfig = {
+    name: 'Web Vault',
+    networks: [
+        {
+            name: 'Github',
+            url: 'https://github.com/Jes015/Web-Vault'
+        }
+    ]
+}
