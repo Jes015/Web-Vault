2ba924a860eaf2d2b27ab217bdb08abba20da932
Jes015 <jes.18.sin.15@gmail.com>
2024-03-06 02:39:45 -0500

    feat: add vercel provider (#8)
    close #8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import node from "@astrojs/node"
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from 'astro/config'
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  })
-})
+  adapter: vercel()
+});
